3705cfc883760d9629262a730c7d1b640cf5eb1f
SpaceEdify <139134025+SpaceEdify@users.noreply.github.com>
2023-07-10 19:42:31 +0300

    Update tvisatdpp.py

--- TVISATDPP/tvisatdpp.py
+++ TVISATDPP/tvisatdpp.py
@@ -1,1 +1,3 @@
-
+#TVISATDPP stands for Technical and Vocational Institute Satellite Data Packaging Protocol. 
+#It is a standard protocol used in the Cube Satellite industry for communication between various electronic devices.
+#Packet Frame: { (&) (00) (Device) (Data) }
